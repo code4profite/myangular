be1ef3d99cc63ed438df2e8e6d4a18fa9cef00a3
trash 9603 <mohamed.hizam111@gmail.com>
2023-01-13 04:40:40 +0100

    calls the listener function of a watch on first $digest'

--- src/scope.js
+++ src/scope.js
@@ -1,7 +1,26 @@
 'use strict';
 
-function Scope(){
+var _ = require('lodash');
 
+function Scope(){
+    this.$$watchers = [];
 }
 
 module.exports = Scope;
+
+Scope.prototype.$watch = function (watchFn,listenerFn) {
+    var watcher = {
+        watchFn: watchFn,
+        listenerFn: listenerFn,
+    }
+    this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function () {
+    this.$$watchers.forEach(watcher => {
+        watcher.listenerFn();
+    });
+    _.forEach(this.$$watchers,function(watcher){
+        watcher.listenerFn();
+    });
+};
